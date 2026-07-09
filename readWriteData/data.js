6cbebd19fc8e370e788c4adfc02185267d160525
שלמה וקסמן <shw4114123@gmail.com>
2026-07-09 16:37:37 +0300

    added the file of the reading and writing to the json files

---
+++ readWriteData/data.js
@@ -0,0 +1,63 @@
+import fsPromises from "fs/promises"
+
+
+
+export async function readcustomerFile() {
+    try {
+        const data = await fsPromises.readFile("./data/customer.json", "utf-8");
+        return JSON.parse(data || [])
+    } catch (e) {
+        console.log(e);
+        return []
+    }
+}
+export async function readOrderFile() {
+    try {
+        const data = await fsPromises.readFile("./data/order.json", "utf-8");
+        return JSON.parse(data || [])
+    } catch (e) {
+        console.log(e);
+        return []
+    }
+}
+export async function readProductFile() {
+    try {
+        const data = await fsPromises.readFile("./data/product.json", "utf-8");
+        return JSON.parse(data || [])
+    } catch (e) {
+        console.log(e);
+        return []
+    }
+}
+
+
+export async function writeCustomerFile(data) {
+    try {
+        const allData = await readcustomerFile()
+        allData.push(data)
+        const write = fsPromises.writeFile(".././data/customer.json", JSON.stringify(allData,null,4), "utf-8");
+        return "succesfuly"
+    } catch (e) {
+        return `error: ${e}`
+    }
+}
+export async function writeProductFile(data) {
+    try {
+        const allData = await readProductFile()
+        allData.push(data)
+        const write = fsPromises.writeFile(".././data/product.json", JSON.stringify(allData,null,4), "utf-8");
+        return "succesfuly"
+    } catch (e) {
+        return `error: ${e}`
+    }
+}
+export async function writeOrderFile(data) {
+    try {
+        const allData = await readOrderFile()
+        allData.push(data)
+        const write = fsPromises.writeFile(".././data/order.json", JSON.stringify(allData,null,4), "utf-8");
+        return "succesfuly"
+    } catch (e) {
+        return `error: ${e}`
+    }
+}
